e130cf34b060de1a9aafcdb8512aad29cd802a41
RJ2004169 <90154107+RJ2004169@users.noreply.github.com>
2024-01-17 12:30:58 +0530

    Update Program.cs
    some more basic questions

--- Program.cs
+++ Program.cs
@@ -10,33 +10,100 @@ namespace CSharpPractice
     {
         public static void Main()
         {
+            PositiveOrNegative();   
+        }
 
-            //// Read 2 numbers and print sum
+        private static void PositiveOrNegative()
+        {
+            Console.Clear();
 
-            //Console.WriteLine("Enter the numbers to add:");
-
-            //int num1 = Convert.ToInt32(Console.ReadLine());
-            //int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
             
-            //int sum = num1 + num2;
+            if(num1 < 0)
+                Console.WriteLine("Negative");
+            else if (num1 > 0)
+                Console.WriteLine("Positive");
+            else
+                Console.WriteLine("Zero");
 
-            //Console.WriteLine("Sum is " + sum);
+            Console.ReadKey();
+        }
 
-            //Console.WriteLine("Value of X is {0} \nValue of Y is {1}\nValue of Sum is {2}", num1, num2, sum);
+        private static void EvenOrOdd()
+        {
+            Console.Clear();
 
-            //Console.ReadKey();
-            
-            //// Read a string and print it
+            Console.WriteLine("Enter the number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if(num1 == 0)
+                Console.WriteLine("Neither odd nor even");
+            else
+                if(num1 == 2)
+                    Console.WriteLine("Odd number");
+                else
+                {
+                    if(num1%2==0)
+                        Console.WriteLine("Even number");
+                    else
+                        Console.WriteLine("Odd number");
+                }
 
-            //Console.Clear();
+            Console.ReadKey();
+        }
 
-            //Console.WriteLine("Enter a string");
-            //string enteredString = Console.ReadLine();
+        private static void LargestNumber()
+        {
+            Console.Clear();
 
-            //Console.WriteLine("String entered is {0}", enteredString);
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
-            //Console.ReadKey();
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
+            if(num1 > num2)
+                Console.WriteLine("Greater is {0}", num1);
+            else
+                Console.WriteLine("Greater is {0}", num2);
+
+            Console.ReadKey();
+        }
+
+        private static void Read2NumbersAndPrintSum()
+        {
+            // Read 2 numbers and print sum
+
+            Console.WriteLine("Enter the numbers to add:");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            Console.WriteLine("Sum is " + sum);
+
+            Console.WriteLine("Value of X is {0} \nValue of Y is {1}\nValue of Sum is {2}", num1, num2, sum);
+
+            Console.ReadKey();
+        }
+
+        private static void ReadAndPrintString()
+        {
+            // Read a string and print it
+
+            Console.Clear();
+
+            Console.WriteLine("Enter a string");
+            string enteredString = Console.ReadLine();
+
+            Console.WriteLine("String entered is {0}", enteredString);
+
+            Console.ReadKey();
+        }
+
+        private static void ReadAndPrintFullName()
+        {
             // Read and print full name
 
             Console.Clear();
@@ -53,5 +120,27 @@ namespace CSharpPractice
 
             Console.ReadKey();
         }
+
+        private static void Swap2Numbers()
+        {
+            // Swap 2 numbers without 3rd variable
+
+            Console.Clear();
+
+            Console.WriteLine("Enter the first number");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            firstNum = firstNum + secondNum;
+            secondNum = firstNum - secondNum;
+            firstNum = firstNum - secondNum;
+
+            Console.WriteLine("Swapped numbers are {0} {1}", firstNum, secondNum);
+
+            Console.ReadKey();
+        }
+
     }
 }
